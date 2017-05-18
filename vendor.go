789ac78aae7c4f7ae0c79a2c1b95c8c322388725
WedgeNix <Mullen.bryce@gmail.com>
2017-05-18 14:01:52 -0700

    Added UseUPC

--- vendor.go
+++ vendor.go
@@ -10,4 +10,5 @@ type Vendor struct {
 	ShareOffPrice bool // Send warning off price if true.
 	WaitingPeriod int  // Maximum days a vendor not provide info.
 	FileDownload  bool // If vendor needs to download file for orders CSV file.
+	UseUPC        bool // If vendor only accepts UPC for email orders.
 }
