39cd939554b8cc0689cb59d909b7ba0b3d6a4935
WedgeNix <Mullen.bryce@gmail.com>
2017-05-18 11:40:25 -0700

    Added WarningTrigger Number of days a vendor can not provide data

--- vendor.go
+++ vendor.go
@@ -1,11 +1,12 @@
 package warehouse
 
-// Vendor holds vendor-specific quantity settings.
+// Vendor holds vendor-specific settings.
 type Vendor struct {
-	WeekendBuffer int
-	WeekdayBuffer int
-	Location      string
-	Email         string
-	PoNum         string
-	ShareOffPrice bool // Send warning Off Price if true
+	WeekendBuffer  int
+	WeekdayBuffer  int
+	Location       string
+	Email          string
+	PoNum          string
+	ShareOffPrice  bool // Send warning off price if true.
+	WarningTrigger int  // Maximum days a vendor not provide info.
 }
