805cca83c8e6e5b91c7fc5752732d11b8b2060c1
WedgeNix <Mullen.bryce@gmail.com>
2017-05-19 10:58:13 -0700

    changed email

--- vendor.go
+++ vendor.go
@@ -5,7 +5,7 @@ type Vendor struct {
 	WeekendBuffer int
 	WeekdayBuffer int
 	Location      string
-	Email         string
+	Email         []string
 	PONum         string
 	ShareOffPrice bool // Send warning off price if true.
 	WaitingPeriod int  // Maximum days a vendor not provide info.
